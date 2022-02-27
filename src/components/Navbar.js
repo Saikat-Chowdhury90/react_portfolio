3ac8038a8d4b27ce685346e0f92409b9ff9d9957
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-02-27 20:18:39 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     <>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">$OUM¥@</a>
+    <a className="navbar-brand" href="/">$OUM¥@</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
