edf069b4425f1e206e4e91b5cc4918b71f314a21
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-02-27 20:14:38 +0530

    movie edit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,7 @@ const Movies = () => {
   return (
    <>
             <div className='video'>
-                 <iframe className='style' src="https://www.youtube.com/embed/43OfzsmknnQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                 
             </div>
            
    </>
