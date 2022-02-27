a97909fb16734fd41395cb7b2ce6a1cb34d5e4a1
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-02-28 00:02:12 +0530

    commit

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import React from 'react'
 import '../styles/ContactScreen.css'
 import twitter from '../images/twitter-brands.svg'
